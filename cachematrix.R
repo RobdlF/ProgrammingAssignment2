18a8080979c0aee4721b6dadb3d6cc36b0decf96
RobdlF <robdlfuente@gmail.com>
2014-10-17 06:36:08 +0100

    -o

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,31 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## The function below sets a matrix 'x' and calls it. It also calculates and calls the inverse of 'x'
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        s <- NULL
+        set <- function(y) {   ## The matrix 'x' is defined as the argument
+                x <<- y        ## for the 'set' function in this environment
+                s <<- NULL     ## 's' has no defined value at this point
+        }
+        get <- function() x                         ## Calls the function
+        setsolve <- function(solve) s <<- solve     ## Calculates the inverse 's'
+        getsolve <- function() s                    ## of the matrix 'x'
+        list(set = set, get = get,                  ## Tests the objects and
+             setsolve = setsolve,                   ## whether they match
+             getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## The next function looks for the inverse of 'x' in the cache. If it does not exist yet, the function solves the
+## matrix 'x' and returns its inverse:
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        s <- x$getsolve()                         ## Asks for the inverse matrix of 'x'
+        if(!is.null(s)) {                         ## If the result of the previous operation is not the inverse of 'x',
+                message("getting cached data")    ## then R 'looks in the cache'
+                return(s)                         ## and returns the inverse of 'x'
+        }
+        data <- x$get()                           ## Retrieves the given matrix 'x' from the environment
+        s <- solve(data, ...)                     ## Defines the inverse 's' of 'x'
+        x$setsolve(s)                             ## Calls the inverse of 'x'
+        s                                         ## Returns the matrix that is the inverse of 'x'
 }
